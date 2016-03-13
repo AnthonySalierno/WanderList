195bdeb2cd3b0b48a24d8d2c140deedbcc8d1174
Anthony Salierno <anthony.salierno@gmail.com>
2016-03-13 16:28:22 -0700

    Define user schema in userModel.js, add npm-debug.log to .gitignore

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.send('GET hit');
@@ -12,5 +12,5 @@ app.post('/login', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log('Example app is listening on port 8080')
+  console.log('Example app is listening on port ' + port);
 });
