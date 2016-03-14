396c74f058f5eba817477e7a95a5d9d377ae777d
Anthony Salierno <anthony.salierno@gmail.com>
2016-03-13 22:29:06 -0700

    Add body-parser, create successful post request to /login

--- server/server.js
+++ server/server.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const db = mongoose.connection;
 
 const port = process.env.PORT || 8080;
 
+db.on('error', console.error());
+db.once('open', () => {
+  const userOne = {
+    username: "AnthonySalierno",
+    password: 'password123',
+  },
+});
+
+mongoose.connect('mongodb://localhost/wanderlist');
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+
+// Routes
 app.get('/', (req, res) => {
-  res.send('GET hit');
+  res.send('GET request successful');
 });
 
 app.post('/login', (req, res) => {
-  res.send('POST hit');
+  res.send('Sucessful login attempt by: ' + req.body.username);
 });
 
 app.listen(port, () => {
